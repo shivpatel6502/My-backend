48fdd353a3b9a18c96c7da87d6efdb63f2c5d2ae
unknown <20it113@charusat.edu.in>
2022-09-07 12:27:33 +0530

    ADDING RDS

--- src/main/java/com/example/mybackend/controller/StudentController.java
+++ src/main/java/com/example/mybackend/controller/StudentController.java
@@ -1,2 +1,61 @@
-package com.example.mybackend.controller;public class StudentController {
+package com.example.mybackend.controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class StudentController {
+
+    List<Student> students = new ArrayList<>(
+            Arrays.asList(
+                    new Student(1, "Tom", "US"),
+                    new Student(2, "Harry", "Canada"),
+                    new Student(3, "Nick", "UK")
+            )
+    );
+
+    // Mappings - URL endpoints
+    // Get the list of all student
+    @GetMapping("/listStudents")
+    public List<Student> getAllStudents(){
+        return students;
+    }
+    // Get the student information
+    @GetMapping("/student/{id}")
+    public Student getStudent(@PathVariable Integer id){
+        for(int i=0; i< students.size(); i++){
+            if(students.get(i).getId()==id){
+                return students.get(i);
+            }
+        }
+        return null;
+    }
+    // Delete the student
+    @DeleteMapping("/student/{id}")
+    public List<Student> deleteStudent(@PathVariable Integer id){
+        for(int i=0; i< students.size(); i++){
+            if(students.get(i).getId()==id){
+                students.remove(i);
+            }
+        }
+        return students;
+    }
+    // Add new student
+    @PostMapping("/student")
+    public List<Student> addStudent(@RequestBody Student student){
+        students.add(student);
+        return students;
+    }
+    // Update the student information
+    @PutMapping("/student/{id}")
+    public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id){
+        for(int i=0; i< students.size(); i++){
+            if(students.get(i).getId()==id){
+                students.get(i).setName(student.getName());
+                students.get(i).setAddress(student.getAddress());
+            }
+        }
+        return students;
+    }
+
 }
--- src/main/java/com/example/mybackend/model/Student.java
+++ src/main/java/com/example/mybackend/model/Student.java
@@ -1,2 +1,37 @@
-package com.example.mybackend.model;public class Student {
+package com.example.mybackend.model;
+package in.ac.charusat.mybackend.controller;
+public class Student {
+    private Integer id;
+    private String name;
+    private String address;
+
+    public Student(Integer id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
